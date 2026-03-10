b5c8edb3411ce393f0b178c1209ad7bb20dc79ae
kyryloivanchenko0-cyber <kyryloivanchenko0@gmail.com>
2026-03-10 12:34:14 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 // Minimal dummy JS
 (function () {
  
-
   // Opprettelse av spillvariabler.
   var player = {
-    navn : "Ole PengeMann",
+    navn : "Ole Pengemann",
     saldo : 10000
   };
-
 
   // spelvariabel
   var game = {
@@ -16,81 +14,146 @@
   }
 
   // Kategori
-  var categori = [
-    {navn : "Transport", id : 1},
-    {navn : "Olje", id : 2},
-    {navn : "Tech", id : 3},
-    {navn : "Diverse", id : 0},
+  var category = [
+    {navn :"Transport",   id: 1},
+    {navn :"Olje",        id: 2},
+    {navn :"Tech",        id:3},
+    {navn :"Diverse",     id:0}
   ]
 
   // Aksjer
-  var stocks =[
-    {navn : "Statoil", price : 1000, available : 500, owned : 0, category : 2},
-    {navn : "Telenor", price : 100, available : 400, owned : 0, category : 3},
-    {navn : "Tine", price : 45.5, available : 250, owned : 0, category : 0},
-    {navn : "PingPanic", price : 0.4, available : 100000, owned : 0, category : 3},
-    {navn : "Apple", price : 3299, available : 2300, owned : 0, category : 3},
+  var stock = [
+    {navn: "Statoil",   price : 1000, available: 500,   owned: 0 ,  category:2},
+    {navn: "Telenor",   price: 100,   available: 400,   owned: 0,   category: 3},
+    {navn: "Tine"   ,   price: 45.5,  available:250,    owned : 0,  category:0},
+    {navn: "PingPanic", price: 0.4,   available:100000, owned: 0,   category:3},
+    {navn: "Apple",     price: 3299,  available: 2300,  owned:0,    category: 3}
   ]
 
 
-  // Her er trykk på ny dag kanppen
-  document.getElementById("NewDay").addEventListener("click", () => 
-    {
-      runNewDay();
+  // Her er trykk på ny dag knappen
+  document.getElementById("newDay").addEventListener("click", () =>
+  { 
+    runNewDay();
   });
 
-
-// Kode som blir kjørt ved ny dag
-function runNewDay()
-{
-// Kalkuler nye priser på aksjer
-// Update days and year
-game.dag = game.dag + 1;
-if(game.dag == 10)
-{
-  game.year = game.year + 1;
-  game.dag = 1;
-}
-// Update the GUI
-updateGUI();
-}
-
-function updateGUI()
-{
-  document.getElementById("dag").innerHTML = game.dag;
-  document.getElementById("year").innerHTML = game.year;
-  document.getElementById("balance").innerHTML = player.saldo;
-  redrawStockList();
-
-}
+  // her er knappane fra aksjelista
+  document.addEventListener("click", function(event){
+    // Kjøp knappen ligger her
+    if(event.target && event.target.tagName === "BUTTON")
+    {
+      if(event.target.id=="kjop")
+      {
+        var stockID = event.target.dataset.stock;
+        buyStock(stockID);
+      }
+      else if(event.target.id=="selg")
+      {
+        var stockID = event.target.dataset.stock;
+        sellStock(stockID);
+      }
+    }
+  });
   
-function redrawStockList()
-{
-  var divlist = document.getElementById("stocklist");
-  divlist.innerHTML = "";
-  for( i = 0; i < stocks.length; i++ )
+  function sellStock(stockID)
   {
+      var currentStock = stock[stockID];
+      var antall = parseInt(prompt("How many stocks do you sell?"));
+      // TODO sjekk kor mange bruker kan selga
+      var totalPris = currentStock.price * antall;
 
-    var info = "";
-    info = info + ((stocks[i].navn + "----<br>"));
-    info = info + (("Price: " + stocks[i].price + "<br>"));
-    info = info + (("Dine aksjer: " + stocks[i].owned + "<br>"));
-    // Kjøp knapp
-    info = info +("<button id='kjop' data-stock='" + i + "'>Kjøp</button>");
-    info = info +("<button id='selg' data-stock='" + i + "'>Selg</button>");
-    // Ny linje
-    info = info + ("<hr>");
-
-    divlist.innerHTML = divlist.innerHTML + info;
+      // salg er godkjent
+      player.saldo = player.saldo + totalPris;
+      stock[stockID].owned = stock[stockID].owned - antall;
+      stock[stockID].available = stock[stockID].available + antall;
+      updateGUI();
   }
 
-}
+  function buyStock(stockID)
+  {
+    var currentStock = stock[stockID];
+    var antall = parseInt(prompt("How many stocks do you want?"));
+
+    // Sjekk kor mange akjser er det mulig å kjøpe
+
+    var totalMaximum = Math.round(player.saldo / currentStock.price);
+    if(totalMaximum > currentStock.available)
+    {
+      totalMaximum = currentStock.available;
+    }
+    if(antall > totalMaximum)
+    {
+      antall = totalMaximum;
+    }
+    // Antall = det antallet aksjer me kan kjøpa.
+    var totalPris = currentStock.price * antall;
+
+    // Kjøp er godkjent
+    player.saldo = player.saldo - totalPris;
+    stock[stockID].owned = stock[stockID].owned + antall;
+    stock[stockID].available = stock[stockID].available - antall;
+    updateGUI();
+  }
+
+  // Kode som blir kjørt ved ny dag
+  function runNewDay()
+  {
+    // Kalkuler nye priser på aksjer
+    calculateNewPrices();
+
+    // update days and year
+    game.dag = game.dag + 1;
+    if(game.dag == 10) // TODO change to 365
+    {
+      game.year = game.year + 1;
+      game.dag = 1;
+    }
+
+    // update the GUI
+    updateGUI();
+  }
+
+  // kalkuler nye priser kvar gang denne blir kjørt.
+  function calculateNewPrices()
+  {
+     for(i = 0; i < stock.length; i++ )
+      {
+        var change = Math.floor(Math.random() * 21) - 10; // mellom -10 + 10
+        stock[i].price = stock[i].price + change;
+      }
+  }
+
+  function updateGUI()
+  {
+    document.getElementById("dag").innerHTML = game.dag;
+    document.getElementById("year").innerHTML = game.year;
+    document.getElementById("balance").innerHTML = player.saldo;
+    redrawStockList();
+  }
+
+  function redrawStockList()
+  {
+      var divList = document.getElementById("stocklist");
+      divList.innerHTML = "";
+
+      for(i = 0; i < stock.length; i++ )
+      {
+        var info = "";
+        info = info + (stock[i].navn + "----");
+        info = info +(" Price: " + stock[i].price );
+        info = info +(" Dine aksjer: " + stock[i].owned);
+        // Kjøp knapp
+        info = info + ("<button id='kjop' data-stock='" + i + "'>Kjøp</button>");
+        info = info + ("<button id='selg' data-stock='" + i + "'>Selg</button>");
+        // Ny linje
+        info = info +("<hr>");
+
+        divList.innerHTML = divList.innerHTML + info;
+      }
+  }
 
 
-
-
-
-
-updateGUI();
+   
+   updateGUI();
 
 })();
